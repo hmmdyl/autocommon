d15b55d1cccdae32c9d2ff664fedbb724d5cf481
Dylan Graham <dylan.graham2000@outlook.com>
2021-08-22 17:48:38 +1000

    {Desktop} Move Optional.match, Result.match out of struct instance due to dual-context deprecation. Optional.match, Result.match are now free functions that accept Optional, Result by ref.

--- source/automotive/option.d
+++ source/automotive/option.d
@@ -1,5 +1,7 @@
 module automotive.option;
 
+import std.traits;
+
 @safe:
 
 /// A result can contain a value (success) or an error code (failure)
@@ -44,21 +46,22 @@ struct Result(T, E)
 	T* unwrapOrNull() 
 	{ return isError ? null : &payload; }
 	alias uwNull = unwrapOrNull;
+}
 
-	/++ Perform pattern match against result or error 
-	+ params:
-	+		onResult = function or lambda to call if result exists (must accept result as param)
-	+		onError = function or lambda to call if error exists (must accept error as param). Allows noreturn ++/
-	auto match(alias onResult, alias onError)()
+/++ Perform pattern match against result or error 
++ params:
++		onResult = function or lambda to call if result exists (must accept result as param)
++		onError = function or lambda to call if error exists (must accept error as param). Allows noreturn ++/
+auto match(alias onResult, alias onError, T)(return ref T t)
+	if(is(T : Result!(R, E), R, E))
+{
+	if(t.isError)
 	{
-		if(isError)
-		{
-			static if(is(ReturnType!onError == noreturn))
-				onError(error);
-			else return onError(error);
-		}
-        else onResult(payload);
+		static if(is(ReturnType!onError == noreturn))
+			onError(t.error);
+		else return onError(t.error);
 	}
+	else onResult(t.payload);
 }
 
 /// An `Optional!T` can either be a `T` type, or `null`
@@ -85,39 +88,41 @@ struct Optional(T)
 	@property bool isNull() const { return !exists; }
 
 	/// Returns a pointer to the data, or panics if null
-	T* unwrapOrPanic()
+	T* unwrapOrPanic() return
 	in(!isNull, typeof(this).stringof ~ " was null")
 	{ return &payload; }
 	/// ditto
 	alias uw = unwrapOrPanic;
 
 	/// Returns a pointer to the data, or null
-	T* unwrapOrNull()
+	T* unwrapOrNull() return
 	{ return isNull ? null : &payload; }
 	/// ditto
 	alias uwNull = unwrapOrNull;
 
 	/// Returns a pointer to the data, or `orelse`
-	T* unwrapOrElse(return ref T orElse)
+	T* unwrapOrElse(return ref T orElse) return
 	{
 		if(isNull) return &orElse;
 		else return &payload;
 	}
 	/// ditto
 	alias uwElse = unwrapOrElse;
+}
 
-	/++ Perform pattern match against result or null 
-	  + params:
-	  +		onResult = function or lambda to call if result exists (must accept result as param)
-	  +		onNull = function or lambda to call if null. Allows noreturn ++/
-	auto match(alias onResult, alias onNull)()
-    {
-        if(exists) return onResult(payload);
-        else
-        {
-            static if(is(ReturnType!onNull == noreturn))
-                onNull();
-            else return onNull();
-        }
-    }
+/++ Perform pattern match against result or null 
++ params:
++		onResult = function or lambda to call if result exists (must accept result as param)
++		onNull = function or lambda to call if null. 
+Allows noreturn ++/
+auto match(alias onResult, alias onNull, T)(return ref T t)
+	if(is(T : Optional!Args, Args...))
+{
+	if(t.exists) return onResult(t.payload);
+	else
+	{
+		static if(is(ReturnType!(onNull) == noreturn))
+			onNull();
+		else return onNull();
+	}
 }
